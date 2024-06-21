4ab778cb884238816b2407e07611dad4a41a8ebc
Dike Goodluck <gudoski30@gmail.com>
2024-06-21 02:46:56 +0100

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -1,13 +1,13 @@
 import { Icons } from "@/components/icons";
 
 export const DATA = {
-  name: "Dillion Verma",
-  initials: "DV",
+  name: "Dike Goodluck",
+  initials: "DG",
   url: "https://dillion.io",
-  location: "San Francisco, CA",
+  location: "Lagos, Nigeria",
   locationLink: "https://www.google.com/maps/place/sanfrancisco",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "Software Engineer . I love building things and helping people. Very active on Twitter.",
   summary:
     "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
   avatarUrl: "/me.png",
